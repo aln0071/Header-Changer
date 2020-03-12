26ab2151c9d194d03c291c29209d326f496765a0
Alan Kuriakose <Alan.Kuriakose.contractor@hilton.com>
2020-03-12 18:22:43 +0530

    Header changer initial commit

---
+++ changeHeader.js
@@ -0,0 +1,11 @@
+// Listen for messages
+//chrome.runtime.onMessage.addListener(function (msg, sender, sendResponse) {
+chrome.runtime.onMessage.addListener(function (msg, sender) {
+    // If the received message has the expected format...
+    if (msg.text === 'change_header') {
+        // Call the specified callback, passing
+        // the web-page's DOM content as argument
+        //sendResponse(document.all[0].outerHTML);
+		document.title = "react js";
+    }
+});
