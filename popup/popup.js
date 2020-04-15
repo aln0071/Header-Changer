1e6e6bef1cf9e66ba1ca13269df714e1c7b38884
Alan <aln0071@gmail.com>
2020-04-15 14:15:34 +0530

    Header change on key press implemented; current header made to display on text field on open up;

--- popup/popup.js
+++ popup/popup.js
@@ -1,23 +1,18 @@
-// chrome.runtime.onMessage.addListener(function (msg, sender) {
-//     if (msg.text === 'change_header') {
-//         console.log("hello");
-//     }
-// });
-
-// console.log("world");
-
 function changeHeader(newTitle) {
     chrome.tabs.query({active: true, currentWindow: true}, (tabs)=>{
         chrome.tabs.sendMessage(tabs[0].id, {type: 'change_header', value: newTitle})
     })
 }
 
-const change = document.getElementById('change');
 const title = document.getElementById('title');
-change.addEventListener('click', (event)=>{
+title.addEventListener('keyup', (event)=>{
     changeHeader(title.value);
 })
 
 window.onload = (event) => {
-    changeHeader('react js');
+    chrome.tabs.query({active: true, currentWindow: true}, (tabs)=>{
+        title.value = tabs[0].title;
+        title.focus();
+        title.select();
+    })
 }
