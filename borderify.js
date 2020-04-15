933d74851a56c712e19424c495fb05ef6ff24da5
Alan <aln0071@gmail.com>
2020-04-15 14:49:13 +0530

    Cleaning up the code

--- borderify.js
+++
@@ -1,1 +0,0 @@
-chrome.browserAction.onClicked.addListener(function(tab) { alert('icon clicked')});
--- changeHeader.js
+++ changeHeader.js
@@ -1,7 +1,5 @@
 chrome.runtime.onMessage.addListener(function (msg, sender) {
     if (msg.type === 'change_header') {
       document.title = msg.value || "";
-    } else if(msg.type === 'get_header') {
-      sendResponse("hello world");
     }
 });
