39d7d57729ffe8d3b87397d04d0c9b3f819cc1fc
Alan <aln0071@gmail.com>
2020-04-15 14:42:08 +0530

    Permissions made more stricter. deleted unwanted comments and background script

--- changeHeader.js
+++ changeHeader.js
@@ -1,11 +1,5 @@
-// Listen for messages
-//chrome.runtime.onMessage.addListener(function (msg, sender, sendResponse) {
-chrome.runtime.onMessage.addListener(function (msg, sender, sendResponse) {
-    // If the received message has the expected format...
+chrome.runtime.onMessage.addListener(function (msg, sender) {
     if (msg.type === 'change_header') {
-        // Call the specified callback, passing
-        // the web-page's DOM content as argument
-        //sendResponse(document.all[0].outerHTML);
       document.title = msg.value || "";
     } else if(msg.type === 'get_header') {
       sendResponse("hello world");
