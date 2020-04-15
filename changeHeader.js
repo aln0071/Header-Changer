34d5ce99c7a6fa639f8e48a1fc77d3fd8edb54d9
Alan <aln0071@gmail.com>
2020-04-15 13:08:08 +0530

    Changes to apply header change on click and change header on submit

--- changeHeader.js
+++ changeHeader.js
@@ -6,6 +6,6 @@ chrome.runtime.onMessage.addListener(function (msg, sender) {
         // Call the specified callback, passing
         // the web-page's DOM content as argument
         //sendResponse(document.all[0].outerHTML);
-      document.title = msg.value || "react js";
+      document.title = msg.value || "";
     }
 });
--- popup/popup.js
+++ popup/popup.js
@@ -6,10 +6,18 @@
 
 // console.log("world");
 
+function changeHeader(newTitle) {
+    chrome.tabs.query({active: true, currentWindow: true}, (tabs)=>{
+        chrome.tabs.sendMessage(tabs[0].id, {type: 'change_header', value: newTitle})
+    })
+}
+
 const change = document.getElementById('change');
 const title = document.getElementById('title');
 change.addEventListener('click', (event)=>{
-    chrome.tabs.query({active: true, currentWindow: true}, (tabs)=>{
-        chrome.tabs.sendMessage(tabs[0].id, {type: 'change_header', value: title.value})
-    })
+    changeHeader(title.value);
 })
+
+window.onload = (event) => {
+    changeHeader('react js');
+}
